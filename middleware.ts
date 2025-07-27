fa4ad3bbd6894bfd277e69e3394c32639788166a
nguyen083 <billbatri085@gmail.com>
2025-07-27 12:10:24 +0700

    update: setup and add dependencies

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// Apply middleware chỉ cho các đường dẫn cần bảo vệ
+export const config = {
+  matcher: ["/home"], // route trang home
+};
